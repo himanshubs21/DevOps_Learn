efa82b68627163c46095a8003534a1ca721f2b8c
Himanshu Bhushan <155674846+himanshubs21@users.noreply.github.com>
2024-09-04 17:34:33 +0530

    Update TimeAnnouncer.sh
     A script that displays a personalized greeting along with the current time
    every minute. For example, "Good morning, Alice! It's Wednesday, 27 March 2024 11:15 AM." (Use
    date and a loop to update every minute).

--- shell_scripts/TimeAnnouncer.sh
+++ shell_scripts/TimeAnnouncer.sh
@@ -1,1 +1,8 @@
-
+#!/bin/bash
+echo "Please enter your name"
+read name
+while true; do
+        timest=$(date "+%A, %d %B %Y %I:%M %p")
+        echo "Good morning, $name! it's $timest."
+        sleep 60
+done
